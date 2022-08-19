e1cea8862bfdbdbdc1059e12e8901800520b7faf
Anthony Manigos <anthony.manigos@serino.com>
2022-08-19 15:12:31 +0800

    Added WindowHandling.test.ts

--- test/UploadFile.test.ts
+++ test/UploadFile.test.ts
@@ -3,8 +3,10 @@ import { chromium } from "playwright";
 
 describe('Upload File Test', () => {
 
-    const filePath0 = '../PlaywrightTypescript/videos/a.webm';
-    const filePath1 = '../PlaywrightTypescript/videos/b.webm';
+    const filePath0 = '../videos/a.webm';
+    const filePath1 = '../videos/b.webm';
+    const filePath2 = '../videos/c.webm';
+    const filePath3 = '../videos/d.webm'
 
     xtest('Upload File', async () => {
         const browser = await chromium.launch({ headless: false, slowMo: 100 })
@@ -30,12 +32,12 @@ describe('Upload File Test', () => {
 
         page.on("filechooser", async (filechooser) => {
             // await filechooser.isMultiple();
-            await filechooser.setFiles([filePath0, filePath1])
+            await filechooser.setFiles([filePath0, filePath1, filePath2, filePath3])
         })
         await page.locator(".example + div#drag-drop-upload").click({force:true})
 
-        // await page.close()
-        // await context.close()
-        // await browser.close()
+        await page.close()
+        await context.close()
+        await browser.close()
     })
 })
